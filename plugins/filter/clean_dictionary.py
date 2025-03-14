6076f6f5044747061d3838ba188fdbf84c606a4a
Bodo Schulz <bodo@boone-schulz.de>
2025-03-14 08:03:59 +0100

    version bump to 2.6.0

---
+++ plugins/filter/clean_dictionary.py
@@ -0,0 +1,51 @@
+# python 3 headers, required if submitting to Ansible
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.utils.display import Display
+
+display = Display()
+
+"""
+Diese Funktion geht rekursiv durch die Struktur (ob Dictionary oder Liste) und entfernt alle Einträge,
+die entweder None, einen leeren String, ein leeres Dictionary, eine leere Liste enthalten.
+
+Für Dictionaries wird jedes Schlüssel-Wert-Paar überprüft, und es wird nur gespeichert, wenn der Wert nicht leer ist.
+Für Listen werden nur nicht-leere Elemente in das Ergebnis aufgenommen.
+
+Es wurde eine Hilfsfunktion `is_empty` eingeführt, die überprüft, ob ein Wert als "leer" betrachtet werden soll.
+
+Diese Funktion berücksichtigt nun explizit, dass boolesche Werte (True und False) nicht als leer betrachtet werden, sondern erhalten bleiben.
+In der is_empty-Funktion wurde eine Überprüfung hinzugefügt, um sicherzustellen, dass die Zahl 0 nicht als leer betrachtet wird.
+Wenn der Wert 0 ist, wird er beibehalten.
+"""
+
+class FilterModule(object):
+    """
+    """
+    def filters(self):
+        return {
+            'remove_empty_values': self.remove_empty_values,
+        }
+
+    def remove_empty_values(self, data):
+        display.v(f"remove_empty_values(self, {data})")
+
+        def is_empty(value):
+            """Überprüfen, ob der Wert leer ist (ignoriere boolesche Werte)."""
+            if isinstance(value, bool):
+                return False  # Boolesche Werte sollen erhalten bleiben
+            if value == 0:
+                return False  # Zahl 0 soll erhalten bleiben
+
+            return value in [None, '', {}, [], False]
+
+        if isinstance(data, dict):
+            # Durch alle Schlüssel-Wert-Paare iterieren
+            return {key: self.remove_empty_values(value) for key, value in data.items() if not is_empty(value)}
+        elif isinstance(data, list):
+            # Leere Listen und leere Elemente entfernen
+            return [self.remove_empty_values(item) for item in data if not is_empty(item)]
+        else:
+            # Andere Typen direkt zurückgeben (einschließlich boolesche Werte)
+            return data
---
+++ plugins/filter/support_tls.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# (c) 2022-2024, Bodo Schulz <bodo@boone-schulz.de>
+
+from __future__ import (absolute_import, print_function)
+__metaclass__ = type
+
+import os
+from ansible.utils.display import Display
+
+display = Display()
+
+
+class FilterModule(object):
+    """
+    """
+
+    def filters(self):
+        return {
+            'support_tls': self.support_tls,
+            'tls_directory': self.tls_directory,
+        }
+
+    def support_tls(self, data):
+        """
+            collabora_config:
+              ssl:
+                enabled: true
+                cert_file: /etc/coolwsd/cert.pem
+                key_file: /etc/coolwsd/key.pem
+                ca_file: /etc/coolwsd/ca-chain.cert.pem
+              storage:
+                ssl:
+                  enabled: ""
+                  cert_file: /etc/coolwsd/cert.pem
+                  key_file: /etc/coolwsd/key.pem
+                  ca_file: /etc/coolwsd/ca-chain.cert.pem
+        """
+        display.v(f"support_tls({data})")
+
+        ssl_data = data.get("ssl", {})
+
+        ssl_enabled = ssl_data.get("enabled", None)
+        ssl_ca = ssl_data.get("ca_file", None)
+        ssl_cert = ssl_data.get("cert_file", None)
+        ssl_key = ssl_data.get("key_file", None)
+
+        if ssl_enabled and ssl_ca and ssl_cert and ssl_key:
+            return True
+        else:
+            return False
+
+    def tls_directory(self, data):
+        """
+        """
+        display.v(f"tls_directory({data})")
+
+        directory = []
+
+        ssl_data = data.get("ssl", {})
+
+        ssl_ca = ssl_data.get("ca_file", None)
+        ssl_cert = ssl_data.get("cert_file", None)
+        ssl_key = ssl_data.get("key_file", None)
+
+        if ssl_ca and ssl_cert and ssl_key:
+            directory.append(os.path.dirname(ssl_ca))
+            directory.append(os.path.dirname(ssl_cert))
+            directory.append(os.path.dirname(ssl_key))
+
+        directory = list(set(directory))
+
+        if len(directory) == 1:
+            result = directory[0]
+
+        display.v(f" = {result}")
+
+        return result
