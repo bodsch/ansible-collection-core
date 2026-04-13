ad79234349703ae1b1bdaec20abfc22ee120f7e2
Bodo Schulz <bodo@boone-schulz.de>
2026-04-13 17:49:17 +0200

    Feature/update modules (#31)
    * update workflows
    * update fail2ban

--- roles/fail2ban/molecule/default/tests/test_default.py
+++ roles/fail2ban/molecule/default/tests/test_default.py
@@ -1,67 +1,14 @@
-import os
-import pprint
+# coding: utf-8
+from __future__ import annotations, unicode_literals
 
 import pytest
-import testinfra.utils.ansible_runner
-from ansible.parsing.dataloader import DataLoader
-from ansible.template import Templar
+from helper.molecule import get_vars, infra_hosts, local_facts
 
-pp = pprint.PrettyPrinter()
+testinfra_hosts = infra_hosts(host_name="all")
 
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ["MOLECULE_INVENTORY_FILE"]
-).get_hosts("all")
+# --- tests -----------------------------------------------------------------
 
-
-def base_directory():
-    cwd = os.getcwd()
-
-    if "group_vars" in os.listdir(cwd):
-        directory = "../.."
-        molecule_directory = "."
-    else:
-        directory = "."
-        molecule_directory = "molecule/{}".format(
-            os.environ.get("MOLECULE_SCENARIO_NAME")
-        )
-
-    return directory, molecule_directory
-
-
-@pytest.fixture()
-def get_vars(host):
-    """ """
-    base_dir, molecule_dir = base_directory()
-
-    file_defaults = "file={}/defaults/main.yml name=role_defaults".format(base_dir)
-    file_vars = "file={}/vars/main.yml name=role_vars".format(base_dir)
-    file_molecule = "file={}/group_vars/all/vars.yml name=test_vars".format(
-        molecule_dir
-    )
-
-    defaults_vars = (
-        host.ansible("include_vars", file_defaults)
-        .get("ansible_facts")
-        .get("role_defaults")
-    )
-    vars_vars = (
-        host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
-    )
-    molecule_vars = (
-        host.ansible("include_vars", file_molecule)
-        .get("ansible_facts")
-        .get("test_vars")
-    )
-
-    ansible_vars = defaults_vars
-    ansible_vars.update(vars_vars)
-    ansible_vars.update(molecule_vars)
-
-    templar = Templar(loader=DataLoader(), variables=ansible_vars)
-    result = templar.template(ansible_vars, fail_on_undefined=False)
-
-    return result
-
+# _facts = local_facts(host=host, fact="fail2ban")
 
 @pytest.mark.parametrize(
     "dirs",
@@ -75,7 +22,6 @@ def get_vars(host):
 def test_directories(host, dirs):
     d = host.file(dirs)
     assert d.is_directory
-    assert d.exists
 
 
 @pytest.mark.parametrize(
@@ -88,5 +34,4 @@ def test_directories(host, dirs):
 )
 def test_files(host, files):
     f = host.file(files)
-    assert f.exists
     assert f.is_file
